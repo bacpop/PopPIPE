ebb57628e2c6f3c6f388987dd4ba8ce385e9d38d
John Lees <lees.john6@gmail.com>
2021-02-15 16:28:11 +0000

    Update rapidnj shaping to use sketchlib functions
    Closes #2

--- scripts/tree_graft.py
+++ scripts/tree_graft.py
@@ -1,24 +1,24 @@
 import dendropy
 
-fulltree = dendropy.Tree.get(path=snakemake.input.overall_tree, 
-                           schema="newick", 
+fulltree = dendropy.Tree.get(path=snakemake.input.overall_tree,
+                           schema="newick",
                            preserve_underscores=True)
 
 for (mltree_file, njtree_file) in zip(snakemake.input['ml_trees'], snakemake.input['nj_trees']):
-    mltree = dendropy.Tree.get(path=mltree_file, 
-                               schema="newick", 
+    mltree = dendropy.Tree.get(path=mltree_file,
+                               schema="newick",
                                preserve_underscores=True)
-    njtree = dendropy.Tree.get(path=njtree_file, 
-                               schema="newick", 
+    njtree = dendropy.Tree.get(path=njtree_file,
+                               schema="newick",
                                preserve_underscores=True)
     mltree.scale_edges(njtree.length() / fulltree.length())
-    
+
     for sample in mltree.taxon_namespace:
         top_node = fulltree.find_node_with_taxon_label(sample.label)
         if top_node:
             matched_node = mltree.find_node_with_taxon_label(sample.label)
             mltree.reroot_at_edge(matched_node.edge, update_bipartitions=False)
-            
+
             # Set a non null edge length for join
             mltree.nodes()[0]._set_edge_length(0)
             mltree.nodes()[1]._set_edge_length(0)
@@ -35,8 +35,8 @@ fulltree.write(path=str(snakemake.output),
             schema="newick",
             suppress_rooting=True,
             unquoted_underscores=True)
-fulltree = dendropy.Tree.get(path=str(snakemake.output), 
-                           schema="newick", 
+fulltree = dendropy.Tree.get(path=str(snakemake.output),
+                           schema="newick",
                            preserve_underscores=True)
 
 # Midpoint seems to take two in dendropy
