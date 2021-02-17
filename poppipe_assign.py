cf1b0a5eb9f706a1d5e9417c1da4edc855d24a09
John Lees <lees.john6@gmail.com>
2021-02-17 15:19:40 +0000

    Update helper script from testing

--- poppipe_assign.py
+++ poppipe_assign.py
@@ -5,6 +5,7 @@ import sys, os
 import pandas as pd
 import re
 import subprocess
+import tempfile
 
 import yaml
 
@@ -12,6 +13,7 @@ import yaml
 combined_rfile = "combined_rfile.txt"
 combined_clusters = "combined_clusters.csv"
 combined_db = "combined_db.h5"
+output_yaml = "config.yml"
 
 # command line parsing
 def get_options():
@@ -26,12 +28,10 @@ def get_options():
     qGroup.add_argument('--output', required=True, help='Argument given to --output')
 
     smGroup = parser.add_argument_group('snakemake files')
-    smGroup.add_argument('--rfile', required=True, help='poppunk_rfile from previous run')
-    smGroup.add_argument('--clusters', required=True, help='poppunk_clusters from previous run')
-    smGroup.add_argument('--h5', required=True, help='poppunk_h5 from previous run')
+    smGroup.add_argument('--config', required=True, help='config.yml from snakemake')
 
     other = parser.add_argument_group('Other options')
-    smGroup.add_argument('--recalculate-random', default=False, action='store_true',
+    other.add_argument('--recalculate-random', default=False, action='store_true',
                           help='Recalculate random matches when joining databases')
 
     return parser.parse_args()
@@ -42,25 +42,39 @@ if __name__ == "__main__":
     # Get command line options
     args = get_options()
 
+    if args.config == output_yaml:
+        sys.stderr.write("Output config file " + output_yaml + " exists\n")
+        output_yaml = tempfile.mkstemp(dir=".", prefix="config.", suffix=".yml")[1]
+        sys.stderr.write("Using " + output_yaml + " instead\n")
+    with open(args.config, 'r') as stream:
+        try:
+            config_in = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(1)
+    rfile = config_in['poppunk_rfile']
+    clusters = config_in['poppunk_clusters']
+    h5 = config_in['poppunk_h5']
+
     # Join files
     with open(combined_rfile, 'w') as rfile_out:
-        with open(args.rfile, 'r') as r1_in:
+        with open(rfile, 'r') as r1_in:
             rfile_out.write(r1_in.read())
         with open(args.query, 'r') as r2_in:
             rfile_out.write(r2_in.read())
 
     # Join clusters
-    ref_clusters = pd.read_table(args.clusters, sep=",",).set_index("Taxon")
+    ref_clusters = pd.read_table(clusters, sep=",",).set_index("Taxon")
     query_clusters = pd.read_table(
         args.output + "/" + os.path.basename(args.output) + "_clusters.csv",
         sep=",",).set_index("Taxon")
-    ref_clusters.append(query_clusters)
-    pd.to_csv(combined_clusters, sep=",")
+    ref_clusters = ref_clusters.append(query_clusters)
+    ref_clusters.to_csv(combined_clusters, sep=",")
 
     # Join DBs
     h5_re = re.compile(r"^(.+)\.h5$")
-    ref_prefix = re.match(h5_re, args.h5).group(1)
-    output_prefix = re.match(h5_re, ref_prefix).group(1)
+    ref_prefix = re.match(h5_re, h5).group(1)
+    output_prefix = re.match(h5_re, combined_db).group(1)
     query_prefix = args.output + "/" + os.path.basename(args.output)
 
     join_cmd = "poppunk_sketch --join --ref-db " + ref_prefix + \
@@ -72,18 +86,11 @@ if __name__ == "__main__":
                         output_prefix,
                        shell=True, check=True)
 
-    with open("config.yaml", 'r') as stream:
-        try:
-            config_in = yaml.safe_load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
-            sys.exit(1)
-
     config_in['poppunk_rfile'] = combined_rfile
     config_in['poppunk_clusters'] = combined_clusters
     config_in['poppunk_h5'] = combined_db
 
-    with open("config.yaml.test", 'w') as stream:
-        output = yaml.dump(config_in)
+    with open(output_yaml, 'w') as stream:
+        output = yaml.dump(config_in, stream)
 
     sys.exit(0)
