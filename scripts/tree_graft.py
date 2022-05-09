51e52b226d604208d777902a40502cb2b06eda0a
John Lees <lees.john6@gmail.com>
2022-05-09 15:36:05 +0100

    Fix ete3 grafting

--- scripts/tree_graft.py
+++ scripts/tree_graft.py
@@ -1,3 +1,4 @@
+import sys
 from ete3 import Tree
 
 def midpoint_root(infile, outfile):
@@ -20,16 +21,24 @@ for (mltree_file, njtree_file) in zip(snakemake.input['ml_trees'], snakemake.inp
     for node in mltree.traverse():
         node.dist /= scale
 
+
+    grafted = False
     for sample in mltree.iter_leaves():
         top_node = fulltree.search_nodes(name=sample.name)
         if top_node:
-            matched_node = mltree.find_node_with_taxon_label(sample.label)
-            mltree.set_outgroup(matched_node)
+            if len(top_node) > 1:
+                sys.stderr.write("Multiple samples with name " + sample.name + "\n")
+                sys.exit(1)
+            else:
+                mltree.set_outgroup(sample.name)
+                top_node.add_child(mltree, dist=0)
 
-            # Set a non null edge length for join
-            top_node.add_child(mltree.nodes, dist=0)
+            grafted = True
             break
+    if not grafted:
+        sys.stderr.write("Could not find graft point in " + mltree_file + "\n")
+        sys.exit(1)
+
 
 fulltree.set_outgroup(fulltree.get_midpoint_outgroup())
 fulltree.write(format=5, outfile=str(snakemake.output))
-
